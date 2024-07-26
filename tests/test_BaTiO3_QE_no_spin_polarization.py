14750f3e742e5d761ce6c16afd96afbbd95ef660
Abigail Poteshman <poteshman@uchicago.edu>
2024-07-26 15:42:29 -0500

    added BaTiO3 QE no spin pol test

---
+++ tests/test_BaTiO3_QE_no_spin_polarization.py
@@ -0,0 +1,21 @@
+from context import BerryFluxDiag as bfd
+
+
+base_path_qe = 'tests/BaTiO3_QE_IO_nospin/'
+np_xml_file = base_path_qe+'non_pol/batio3_np.xml'
+pol_xml_file = base_path_qe+'pol/batio3_pol.xml'
+np_wfcn_path = base_path_qe+'non_pol/batio3_np.save/'
+pol_wfcn_path = base_path_qe+'pol/batio3_pol.save/'
+np_out_file = base_path_qe+'non_pol/batio3_np.out'
+
+qe_dict = bfd.berry_flux_diag.qe_parser(pol_xml_file,
+                        np_xml_file,
+                        pol_wfcn_path,
+                        np_wfcn_path,
+                        np_out_file)
+
+BaTiO3_Overlaps = bfd.berry_flux_diag.Overlaps(qe_dict)
+
+pol, _ = BaTiO3_Overlaps.compute_polarization()
+print(pol)
+print("test complete")
