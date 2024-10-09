6e404f7714c79aa3e1bc15b2da0d5d0a06a79f90
Abigail Poteshman <poteshman@uchicago.edu>
2024-10-09 16:05:17 -0500

    generate QE.in files from POSCAR/pymatgen structs

--- preprocess_QE_input/preprocess_example.py
+++ preprocess_QE_input/preprocess_example.py
@@ -3,6 +3,32 @@ from preprocess_funcs import *
 def main():
     CrO3_path = 'CrO3_fe_db_cells/'
     CrO3_np_trans_struct, CrO3_pol_struct, CrO3_max_disp = make_one_translation(CrO3_path)
+
+    file_path = 'CrO3_fe_db_cells/'
+    out_path = file_path
+    material = 'CrO3'
+    pseudo_dir = '/global/homes/p/pabigail/qe_pseudos/' # change to path to pseudo directory
+    kshift = (0, 0, 0)
+    sym = False 
+    kpoints_grid = (7, 7, 7)
+    species_dict = {"species": ["Cr", "O"], # update with appropriate information
+                "masses": [51.9961, 15.9994],
+                "psuedos": ["Cr_ONCV_PBE-1.0.upf", # these need to be in the pseudo_dir
+                            "O_ONCV_PBE-1.0.upf"]}
+
+    # np_trans_1
+    poscar_file = file_path+'POSCAR_np_trans_1'
+    tag = "np_trans_1"
+    poscar_to_qe_io_scf_nomag(file_path, poscar_file, material, tag, sym, pseudo_dir,
+                          kpoints_grid, kshift, species_dict, out_path)
+
+    # pol_orig
+    poscar_file = file_path+'POSCAR_pol_orig'
+    tag = "pol_orig"
+    poscar_to_qe_io_scf_nomag(file_path, poscar_file, material, tag, sym, pseudo_dir,
+                          kpoints_grid, kshift, species_dict, out_path)
+
+
     return 0
 
 if __name__ == '__main__':
--- preprocess_QE_input/preprocess_funcs.py
+++ preprocess_QE_input/preprocess_funcs.py
@@ -54,3 +54,120 @@ def make_one_translation(folder_path):
     np_trans_1.to(filename=save_file_1, fmt='POSCAR')
     
     return np_trans_1, pol_struct, disp_1
+
+
+
+def poscar_to_qe_io_scf_nomag(file_path, poscar_file, material, tag, sym,
+                          pseudo_dir, kpoints_grid, kshift, species_dict, out_path):
+    '''
+    file_path: file where poscar is located and where QE.in file will be written to
+    poscar_file: name of poscar file
+    material: name of material in string form, ex "BaTiO3"
+    tag: for naming prefix and QE input file ex "np_orig" or "trans_1" or "interp_1"
+    sym: boolean (true or false)
+    pseudo_dir: location of psuedopotential files
+    kpoints_grid: ex (5, 5, 5)
+    kshift: either (0, 0, 0) or (1, 1, 1)
+    species_dict: contains atoms and materials
+    out_path: where to write file out to 
+    '''
+    
+    poscar = Poscar.from_file(poscar_file)
+    structure = poscar.structure
+    k1, k2, k3 = kpoints_grid
+    name = material+'_'+tag
+
+    control_dict = {
+    "calculation": "scf",
+    "prefix": name,
+    "pseudo_dir": pseudo_dir,
+    "outdir": 'calculations_'+str(k1)+str(k2)+str(k3)+'/',
+    "verbosity": "high"
+    }
+
+    system_dict = {
+    "ibrav": 0,
+    "ecutwfc": 100,
+    "input_dft": "pbe",
+    "nat": structure.num_sites,
+    "ntyp": len(structure.composition.elements),
+    }
+    
+    if not sym:
+        system_dict["nosym"] = True
+        system_dict["noinv"] = True
+
+    
+    electrons_dict = {
+    "conv_thr": 1e-8,
+    "mixing_beta": 0.7,
+    "mixing_mode": 'plain',
+    "mixing_ndim": 8,
+    "diagonalization": 'david'
+    }
+    
+    if sym:
+        sym_str = "sym"
+    else:
+        sym_str = "nosym"
+
+    filename = out_path+name+'_k'+str(k1)+str(k2)+str(k3)+'_scf_'+sym_str+'.in'
+
+    with open(filename, "w") as f:
+    
+        f.write("&CONTROL\n")
+        for key, value in control_dict.items():
+            if isinstance(value, str):
+                f.write(f"  {key} = '{value}'\n")
+            else:
+                f.write(f"  {key} = {value}\n") 
+        f.write("  /\n")
+    
+        f.write(" &SYSTEM\n")
+        for key, value in system_dict.items():
+            if isinstance(value, bool):
+                value_str = ".TRUE." if value else ".FALSE."
+                f.write(f"  {key} = {value_str}\n")
+            elif isinstance(value, str):
+                f.write(f"  {key} = '{value}'\n")
+            else:
+                f.write(f"  {key} = {value}\n")
+        f.write("  /\n")
+    
+        f.write("  &ELECTRONS\n")
+        for key, value in electrons_dict.items():
+            if key == "conv_thr":
+                # Special case for scientific notation in Quantum Espresso style
+                f.write(f"  {key} = {value:.1e}".replace('e', 'd') + "\n")
+            elif isinstance(value, bool):
+                value_str = ".TRUE." if value else ".FALSE."
+                f.write(f"  {key} = {value_str}\n")
+            elif isinstance(value, str):
+                f.write(f"  {key} = '{value}'\n")
+            else:
+                f.write(f"  {key} = {value}\n")
+        # Add custom adaptive_thr parameter
+        f.write("  adaptive_thr = .TRUE.\n")
+        f.write("  /\n")
+
+        f.write("ATOMIC_SPECIES\n")
+        for i in range(len(species_dict["species"])):
+            species = species_dict["species"][i]
+            mass = species_dict["masses"][i]
+            pseudo = species_dict["psuedos"][i]
+            f.write(f"  {species}  {mass:.4f} {pseudo}\n")
+    
+        f.write("ATOMIC_POSITIONS crystal\n")
+        for site in structure.sites:
+            species = site.specie.symbol
+            frac_coords = site.frac_coords
+            f.write(f"{species} {frac_coords[0]:.6f} {frac_coords[1]:.6f} {frac_coords[2]:.6f}\n")
+
+        f.write("K_POINTS automatic\n")
+        f.write(f"  {kpoints_grid[0]} {kpoints_grid[1]} {kpoints_grid[2]} {kshift[0]} {kshift[1]} {kshift[2]}\n")
+    
+        f.write("CELL_PARAMETERS angstrom\n")
+        for vec in structure.lattice.matrix:
+            f.write(f"  {vec[0]:.6f} {vec[1]:.6f} {vec[2]:.6f}\n")
+        
+    return 0
