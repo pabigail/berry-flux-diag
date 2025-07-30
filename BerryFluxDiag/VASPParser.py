fad2186b1ec04993787630a415380461631b6403
Abby Poteshman <78704917+pabigail@users.noreply.github.com>
2025-07-30 16:09:41 -0700

    updated with VASP psuedo corrections

--- BerryFluxDiag/VASPParser.py
+++ BerryFluxDiag/VASPParser.py
@@ -1,9 +1,13 @@
 from pymatgen.core.structure import Structure
-from pymatgen.io.vasp.outputs import Wavecar, Kpoints, Potcar
+from pymatgen.io.vasp import Vasprun
+from pymatgen.io.vasp.outputs import Wavecar, Kpoints, Potcar, Outcar # clean later
 from pymatgen.analysis.ferroelectricity.polarization import zval_dict_from_potcar
+from pawpyseed.core.wavefunction import Wavefunction, CoreRegion
+from pawpyseed.core.momentum import MomentumMatrix
+from pawpyseed.core import pawpyc
 import numpy as np
 import BerryFluxDiag.utils as utils
-
+from pathlib import Path
 
 def get_band_filling_from_wavecar_nospin(wavecar, tol):
 
@@ -22,7 +26,6 @@ def get_band_filling_from_wavecar_nospin(wavecar, tol):
 
     return max_band_fill
 
-
 def get_band_filling_from_wavecar_spinpol(wavecar, tol, spin_channel):
 
     max_band_fill = 0
@@ -40,6 +43,8 @@ def get_band_filling_from_wavecar_spinpol(wavecar, tol, spin_channel):
     return max_band_fill
 
 
+
+# remove following
 def get_wfcn_dict_from_vasp(wavecar, kpoint_list, spin_pol):
     # key of wfcn dict is a k-point
     # each k-point has associated dict with coeffs and gvecs
@@ -86,17 +91,106 @@ def get_wfcn_dict_from_vasp(wavecar, kpoint_list, spin_pol):
     else:
         return wfcn_dict, max_band_fill
 
-
-def vasp_parser(pol_POSCAR, np_POSCAR, pol_WAVECAR, np_WAVECAR, POTCAR):
+# only works for non-spin-polarized wavefunctions...
+def get_wfcn_data_from_vasp_pawpy(wavecar_file, potcar_file, vasprun_file, max_band_fill, cutoff=1000):
     
+    # wavefunctions and momentum matrix objects
+    vr = Vasprun(vasprun_file)
+    structure = vr.final_structure
+    dim = np.array([vr.parameters["NGX"], vr.parameters["NGY"], vr.parameters["NGZ"]])
+    symprec = vr.parameters["SYMPREC"]
+    potcar = Potcar.from_file(potcar_file)
+    pwf = pawpyc.PWFPointer(wavecar_file, vr)
+    wf = Wavefunction(structure, pwf, CoreRegion(potcar), dim, symprec, True)
+    mm = MomentumMatrix(wf, cutoff)
+
+    # g-point grid and number of k-points
+    gpoints = mm.momentum_grid
+    nkpts = wf.nwk
+    # nbands = wf.nband
+    nbands = max_band_fill
+    TOL = 1e-6
+    ngpoints = len(gpoints)
+
+    wfc_data = np.zeros((nbands, nkpts, ngpoints), dtype=complex)
+
+    for kpt_idx in range(nkpts):
+        for band_idx in range(nbands):
+            wfc_data[band_idx, kpt_idx, :] = mm.get_reciprocal_fullfw(band_idx, kpt_idx, 0)
+    
+    return gpoints, ngpoints, wfc_data
+
+def wfcn_dict_from_pawpy(wavecar_file, potcar_file, vasprun_file, max_band_fill, kpoint_list):
+    """
+    Generate wfcn_dict from PAWPyseed wavefunction data.
+    The dictionary maps each k-point to its corresponding wavefunction coefficients and g-vectors.
+    
+    Inputs:
+        wavecar_file (str): Path to WAVECAR
+        potcar_file (str): Path to POTCAR
+        vasprun_file (str): Path to vasprun.xml
+        kpoint_list (List[np.ndarray]): List of k-points (fractional coordinates)
+
+    Returns:
+        dict: {kpt (tuple): {'wfcn': array(nbands, ngpoints), 'gvecs': array(ngpoints, 3)}}
+    """
+    # Call the original function to get PAW-corrected data
+    gvecs, ngpoints, wfc_data = get_wfcn_data_from_vasp_pawpy(
+        wavecar_file, potcar_file, vasprun_file, max_band_fill
+    )
+
+    nkpts = len(kpoint_list)
+    nbands = wfc_data.shape[0]
+
+    wfcn_dict = {}
+
+    for kpt_idx in range(nkpts):
+        # Get wavefunction coefficients for this k-point (shape: nbands, ngpoints)
+        wfcn = wfc_data[:, kpt_idx, :]  # shape: (nbands, ngpoints)
+        coeff_gvec_dict = {
+            'wfcn': wfcn,
+            'gvecs': gvecs  # same for all k-points in pawpyseed's MomentumMatrix
+        }
+        wfcn_dict[tuple(kpoint_list[kpt_idx])] = coeff_gvec_dict
+
+    return wfcn_dict
+
+
+def get_band_filling_from_outcar(outcar, spin_pol, occ_tol=1e-6):
+    # outcar is a pymatgen.io.vasp.outputs Outcar object
+    outcar.read_eigenval()
+    
+    if spin_pol:
+        occ_up = np.array(outcar.occupancies[0])
+        occ_down = np.array(outcar.occupancies[1])
+        fill_up = np.sum(occ_up[0] > occ_tol)
+        fill_down = np.sum(occ_down[0] > occ_tol)
+        return int(fill_up), int(fill_down)
+    else:
+        occ = np.array(outcar.occupancies)
+        fill = np.sum(occ[0] > occ_tol)
+        return int(fill)
+
+def vasp_parser(pol_POSCAR, np_POSCAR, pol_WAVECAR, np_WAVECAR, POTCAR, pol_directory, np_directory):
+   
+    
+     
     pol_struct = Structure.from_file(pol_POSCAR)
     np_struct = Structure.from_file(np_POSCAR)
 
-    pol_wavecar = Wavecar(pol_WAVECAR)
-    np_wavecar = Wavecar(np_WAVECAR)
+    pol_wavecar = Wavecar(pol_WAVECAR, vasp_type="std")
+    np_wavecar = Wavecar(np_WAVECAR, vasp_type="std")
 
     potcar = Potcar.from_file(POTCAR)
     zval_dict = zval_dict_from_potcar(potcar)
+
+    pol_wfcn = Wavefunction.from_directory(pol_directory)
+    np_wfcn = Wavefunction.from_directory(np_directory)
+    
+    pol_vasprun = Path(pol_directory) / 'vasprun.xml'
+    np_vasprun = Path(np_directory) / 'vasprun.xml'
+
+    # pol_wfcn_pawpy = get_wfcn_dict_from_vasp_pawpy(pol_WAVECAR, POTCAR,  pol_vasprun)
 
     # determine whether spin-polarized from shape of coefficient array
     if len(np.shape(np.array(pol_wavecar.coeffs, dtype=object))) == 3:
@@ -125,17 +219,34 @@ def vasp_parser(pol_POSCAR, np_POSCAR, pol_WAVECAR, np_WAVECAR, POTCAR):
 
 
     if spin_pol:
-        pol_wfcn_dict, pol_band_fill_up, pol_band_fill_down = get_wfcn_dict_from_vasp(pol_wavecar, kpoint_list, spin_pol)
-        np_wfcn_dict, np_band_fill_up, np_band_fill_down = get_wfcn_dict_from_vasp(np_wavecar, kpoint_list, spin_pol)
+        pol_wfcn_dict, _, _ = get_wfcn_dict_from_vasp(pol_wavecar, kpoint_list, spin_pol)
+        np_wfcn_dict, _, _ = get_wfcn_dict_from_vasp(np_wavecar, kpoint_list, spin_pol)
     else:
-        pol_wfcn_dict, pol_band_fill = get_wfcn_dict_from_vasp(pol_wavecar, kpoint_list, spin_pol)
-        np_wfcn_dict, np_band_fill = get_wfcn_dict_from_vasp(np_wavecar, kpoint_list, spin_pol)
+        pol_wfcn_dict, _ = get_wfcn_dict_from_vasp(pol_wavecar, kpoint_list, spin_pol)
+        np_wfcn_dict, _ = get_wfcn_dict_from_vasp(np_wavecar, kpoint_list, spin_pol)
+
+    TOL = 1e-6    
+
+    if spin_pol:
+        spin_up = 0
+        spin_down = 1
+        pol_band_fill_up = get_band_filling_from_wavecar_spinpol(pol_wavecar, TOL, spin_up)
+        pol_band_fill_down = get_band_filling_from_wavecar_spinpol(pol_wavecar, TOL, spin_down)
+        np_band_fill_up = get_band_filling_from_wavecar_spinpol(np_wavecar, TOL, spin_up)
+        np_band_fill_down = get_band_filling_from_wavecar_spinpol(np_wavecar, TOL, spin_down)
+    else:
+        pol_band_fill = get_band_filling_from_wavecar_nospin(pol_wavecar, TOL)
+        np_band_fill = get_band_filling_from_wavecar_nospin(np_wavecar, TOL)    
     
+    pol_wfcn_dict_pawpy = wfcn_dict_from_pawpy(pol_WAVECAR, POTCAR, pol_vasprun, pol_band_fill, kpoint_list)
+    np_wfcn_dict_pawpy = wfcn_dict_from_pawpy(np_WAVECAR, POTCAR, np_vasprun, np_band_fill, kpoint_list)
+
     vasp_parse_dict = utils.empty_parse_dict()
     vasp_parse_dict['pol_struct'] = pol_struct
     vasp_parse_dict['np_struct'] = np_struct
-    vasp_parse_dict['pol_wfcn_dict'] = pol_wfcn_dict
-    vasp_parse_dict['np_wfcn_dict'] = np_wfcn_dict
+    vasp_parse_dict['pol_wfcn_dict'] = pol_wfcn_dict_pawpy
+    vasp_parse_dict['np_wfcn_dict'] = np_wfcn_dict_pawpy
+    vasp_parse_dict['kpoint_list'] = kpoint_list
     vasp_parse_dict['zval_dict'] = zval_dict
     vasp_parse_dict['ES_code'] = 'VASP'
     vasp_parse_dict['spin_pol'] = spin_pol
